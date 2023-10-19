c6f3c61385c520a9ddffb7b828064500e50d8b63
Berat Dogan <berat.dogan.3458@gmail.com>
2023-10-19 22:37:47 +0300

    background component

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 import Clock from '@/components/Clock/clock'
 import Timer from '@/components/Timer/timer'
 import ToDoList from '@/components/ToDoList/ToDoList';
-import Image from 'next/image'
+import Background from '@/components/Background';
 import { IoMdSettings } from 'react-icons/io'
 import { AiOutlineTool } from 'react-icons/ai'
 import { MdOutlineWallpaper } from 'react-icons/md'
@@ -40,14 +40,7 @@ export default function Home() {
 
   return (
     <main>
-      <backgorund className='h-screen w-full fixed top-0 left-0 z-0'>
-        <Image
-          src='/2471316.gif'
-          alt='Image'
-          layout='fill'
-          style={{ objectFit: 'cover', width: '100%', height: '100%' }}
-        />
-      </backgorund>
+      <Background />
       <tools className='z-10 absolute'>
         <div className={clock ? 'hidden' : ''}>
           <Clock />
@@ -61,8 +54,8 @@ export default function Home() {
       </tools>
       <settings className='absolute top-[3%] right-[2%] z-20'>
         <div className='w-fit'>
-          <button onClick={() => setState(!state)} className=''>
-            <IoMdSettings size={50} />
+          <button onClick={() => setState(!state)}>
+            <IoMdSettings size={50} className='text-[#000] opacity-75' />
           </button>
           <div className={state ? 'flex flex-col items-center mt-5' : 'hidden'}>
             <ul className='flex flex-col gap-5'>
---
+++ components/Background.jsx
@@ -0,0 +1,11 @@
+export default function background() {
+  return (
+    <backgorund className='h-screen w-full fixed top-0 left-0 z-0'>
+      <img
+        src='/2471316.gif'
+        alt='Image'
+        className='w-full h-full'
+      />
+    </backgorund>
+  )
+}
