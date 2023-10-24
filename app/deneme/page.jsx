0b168bc62b8f38a92d8024f5dd4892c938626c62
Berat Dogan <berat.dogan.3458@gmail.com>
2023-10-24 17:35:03 +0300

    spotify api testing

--- app/deneme/page.jsx
+++ app/deneme/page.jsx
@@ -1,74 +1,86 @@
 "use client";
 
 import { useState, useEffect } from 'react';
-import { FiSettings } from 'react-icons/fi';
+import axios from 'axios';
+import { render } from 'react-dom';
 
-export default function page() {
+export default function Page() {
   const CLIENT_ID = 'a19c9fda753748628ea9b9df210a8109';
-  const CLIENT_SECRET = '21ab9e8cc2e6414f8d2d625bee663852';
-  const [state, setState] = useState(false);
+  const REDIRECT_URI = 'http://localhost:3000/deneme';
+  const AUTH_ENDPOINT = 'https://accounts.spotify.com/authorize';
+  const RESPONSE_TYPE = 'token';
+
   const [token, setToken] = useState('');
-
-  const handleButton = () => {
-    setState(!state);
-  };
-
-  var apiParameters = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `Bearer ${token}`
-    }
-  }
-
-  // Spotify API endpoint'ini tanımlayın
-  const endpoint = 'https://api.spotify.com/v1/me/playlists';
-
-  // API isteğini yapın
-  fetch(endpoint, {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${token}`,
-    },
-  })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data); // Yanıt verilerini işleyin
-    })
-    .catch(error => {
-      console.error('Hata:', error);
-    });
+  const [searchKey, setSearchKey] = useState('');
+  const [artists, setArtists] = useState([]);
 
   useEffect(() => {
-    // API Access Token
-    var authParameters = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
+    const hash = window.location.hash;
+    let token = window.localStorage.getItem('token');
+
+    if (!token && hash) {
+      token = hash.substring(1).split('&').find(elem => elem.startsWith('access_token')).split('=')[1];
+
+      window.location.hash = '';
+      window.localStorage.setItem('token', token);
     }
-    fetch('https://accounts.spotify.com/api/token', authParameters)
-      .then(response => response.json())
-      .then(data => setToken(data.access_token))
-      .catch(err => console.log(err));
+
+    setToken(token);
   }, []);
 
-  return (
-    <main className='absolute top-[3%] right-[2%]'>
-      <div className='w-fit'>
-        <button onClick={handleButton}>
-          <FiSettings size={50} />
-        </button>
-        <div className={state ? 'flex flex-col items-center mt-5' : 'hidden'}>
-          <ul className='flex flex-col gap-5'>
-            <li>b</li>
-            <li>b</li>
-            <li>b</li>
-            <li>b</li>
-          </ul>
+  const logout = () => {
+    setToken('');
+    window.localStorage.removeItem('token');
+  }
+
+  const searchArtists = async (e) => {
+    e.preventDefault();
+    // setArtists([]);
+    const { data } = await axios.get('https://api.spotify.com/v1/search', {
+      headers: {
+        'Authorization': `Bearer ${token}`
+      },
+      params: {
+        q: searchKey,
+        type: 'artist'
+      }
+    })
+
+    setArtists(data.artists.items);
+  }
+
+  const renderArtits = () => {
+    return artists.map(artist => {
+      return (
+        <div key={artist.id}>
+          {artist.images.length ? <img width={'100%'} src={artist.images[0].url} alt={artist.name} /> : <div>No Image</div>}
+          <p>{artist.name}</p>
         </div>
+      )
+    })
+  }
+
+  return (
+    <div className="grid place-items-center h-screen">
+      <div className="flex flex-col gap-5 items-center">
+        <h1>Spotify Project Nextjs v13</h1>
+        {!token ?
+          <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
+          :
+          <button onClick={logout}>Logout</button>
+        }
+
+        {token ?
+          <form onSubmit={searchArtists}>
+            <input type="text" onChange={e => setSearchKey(e.target.value)} />
+            <button type={'submit'}>Search</button>
+          </form>
+
+          : <p>You need to login to search</p>
+        }
+
+        {renderArtits()}
       </div>
-    </main>
+    </div>
   );
 }
