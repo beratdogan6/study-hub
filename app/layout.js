e29c18e18dff6e92b97a8ccc5b5bf5b8496515cf
Berat Dogan <berat.dogan.3458@gmail.com>
2024-06-02 18:11:33 +0300

    vercel analytics added

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,7 @@ export default function RootLayout({ children }) {
       <body className='font-poppins'>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
