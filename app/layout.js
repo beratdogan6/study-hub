84181cd4dbc17fc352ade0c326800b4ab8d69975
Berat Dogan <berat.dogan.3458@gmail.com>
2024-06-02 18:02:49 +0300

    speedinsights

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <div className='font-poppins'>
           {children}
         </div>
